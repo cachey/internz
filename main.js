96386e51ef3a88c452175e94b8a475c84985ecf8
Kevin Shi <k.shi@me.com>
2015-08-11 05:27:58 -0400

    Starting a refactoring

---
+++ main.js
@@ -0,0 +1,33 @@
+var aggToPct = function(agg) {
+  return -220292 
+  + 15453.5 * agg 
+  - 432.364 * Math.pow(agg, 2) 
+  + 6.02704 * Math.pow(agg, 3) 
+  - 0.0418341 * Math.pow(agg, 4) 
+  + 0.000115637 * Math.pow(agg, 5);
+}
+var gaussian = function(mean, variance){ return new Gaussian(mean, variance); };
+
+var internz = gaussian(3.5, 1.0); 
+  document.getElementById("twoStep").onclick = function() {
+
+};
+var calcZScoreFromPercentile = function() {
+  document.getElementById("pile").className = (parseFloat(document.getElementById("pile").value) === NaN) ? "form-control err" : "form-control";
+
+  var score = internz.ppf(parseFloat(document.getElementById("pile").value * 0.01));
+  document.getElementById("results").innerHTML = "zScore: " + (Math.round(score * 100) / 100).toString();
+};
+
+document.getElementById("oneStep").onclick = calcZScoreFromPercentile;
+
+document.getElementById("twoStep").onclick = function() {
+  var agg = parseFloat(document.getElementById("med2").value)
+  + (2 * parseFloat(document.getElementById("med4").value));
+  agg = agg / 3;
+  var res = Math.round(aggToPct(agg) * 100) / 100;
+  res = res < 0 ? 1 : res;
+  res = res > 100 ? 99.9 : res;
+  document.getElementById("pile").value = res;
+  calcZScoreFromPercentile();
+}
